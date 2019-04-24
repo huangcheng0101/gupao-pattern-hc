3e561721f656bcd6b5576d4e1ccc722e66d9d4f6
huangcheng <hc10512@163com>
2019-04-25 03:00:34 +0800

    add junit test

--- src/test/java/com/gupao/pattern/factory/simplefactory/SimpleFactoryTest.java
+++ src/test/java/com/gupao/pattern/factory/simplefactory/SimpleFactoryTest.java
@@ -1,8 +1,17 @@
 package com.gupao.pattern.factory.simplefactory;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by huangcheng on 2019/4/25.
  */
 public class SimpleFactoryTest
 {
+    @Test
+    public void testSimpleFactory() {
+        String expect = "Hello World";
+        String result = "Hello World";
+        Assert.assertEquals(expect, result);
+    }
 }
